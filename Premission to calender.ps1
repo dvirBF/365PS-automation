784505d9ed709cf4664901b82afe955d50721791
dvirBF <93584738+dvirBF@users.noreply.github.com>
2023-03-31 08:03:51 +0300

    Calender Permission
    Here's a PowerShell script that installs the Exchange Online Management module, updates it, connects to Exchange Online, gets mailbox folder permissions for a specified user's calendar, and adds a new mailbox folder permission to another user's calendar

---
+++ Premission to calender.ps1
@@ -0,0 +1,14 @@
+﻿# Install Exchange Online Management module
+Install-Module -Name ExchangeOnlineManagement -Force
+
+# Update Exchange Online Management module
+Update-Module ExchangeOnlineManagement
+
+# Connect to Exchange Online
+Connect-ExchangeOnline
+
+# Get mailbox folder permissions for specified user's calendar
+Get-MailboxFolderPermission "mailbox name:\calendar
+
+# Add new mailbox folder permission to another user's calendar
+Add-MailboxFolderPermission -Identity "mailbox name":\calendar -User "mailbox name" -AccessRights Reviewer
